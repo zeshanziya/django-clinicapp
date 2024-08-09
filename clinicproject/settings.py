959d856771de156395ac678663473f34862615b7
zeshanziya <zeshan.ziya@axelerant.com>
2024-08-09 11:40:11 +0530

    add logo

--- clinicproject/settings.py
+++ clinicproject/settings.py
@@ -128,6 +128,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
 
 STATIC_URL = 'static/'
+STATICFILES_DIRS = [BASE_DIR / "static"]  # This line includes your global static directory
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
